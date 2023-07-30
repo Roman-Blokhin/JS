d041c9cbf4d0e69a8210bfc306b3213f60bda43d
Roman <11-roman@mail.ru>
2023-07-30 17:50:43 +0300

    создал файл html и js для изучения js

--- lesson_1.js
+++ lesson_1.js
@@ -1,1 +1,7 @@
-console.log ('Hello Roman')
+let num = 42 // используем let для определения переменной
+let firstName = 'Roman'
+const isProgrammer = true
+
+num = 10 // если переменная есть, то заново let мы не пишем
+
+ 
