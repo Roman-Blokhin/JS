8a3841d82f23fcdeadae511c55835c0860e0720e
Roman <11-roman@mail.ru>
2023-07-31 22:07:22 +0300

    document.getElementById, Number(), textContent

--- lesson_1.js
+++ lesson_1.js
@@ -4,4 +4,14 @@ const isProgrammer = true
 
 num = 10 // если переменная есть, то заново let мы не пишем
 
-// тест
+// alert (firstName) // агрессивный способ вывести информацию на экран
+
+console.log ("Test:", firstName) // выводит информацию в консоль
+
+// создаем константу, которая подтягивает значение по id из файла html
+const resultElement = document.getElementById('result')
+const input1 = document.getElementById('input1') // первое число
+const input2 = document.getElementById('input2') // второе число
+const sum = Number(input1.value) + Number(input2.value) // константа суммы + изменение тепа на числовой
+resultElement.textContent = sum // вывод результата на экране
+console.log(resultElement.textContent) // вывод результата в консоли
