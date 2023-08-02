f67f486dd6d492c23ee043b503e5a5dd2dfd34bf
Roman <11-roman@mail.ru>
2023-08-02 10:29:02 +0300

    fix errors, конпка - выполнить работает корректно

--- lesson_1.js
+++ lesson_1.js
@@ -1,24 +1,22 @@
-let num = 42 // используем let для определения переменной
-let firstName = 'Roman'
-const isProgrammer = true
-
-num = 10 // если переменная есть, то заново let мы не пишем
-
+// let num = 42 // используем let для определения переменной
+// let firstName = 'Roman'
+// const isProgrammer = true
+// num = 10 // если переменная есть, то заново let мы не пишем
 // alert (firstName) // агрессивный способ вывести информацию на экран
+//console.log ("Test:", firstName) // выводит информацию в консоль
 
-console.log ("Test:", firstName) // выводит информацию в консоль
-
-// создаем константу, которая подтягивает значение по id из файла html
+// создаем константы, которая подтягивает значение по id из файла html
 const resultElement = document.getElementById('result')
 const input1 = document.getElementById('input1') // первое число
 const input2 = document.getElementById('input2') // второе число
 const sum = Number(input1.value) + Number(input2.value) // константа суммы + изменение тепа на числовой
-const submitBtn = document.getElementById('submit') // константа для кнопки - Выполнить
+const submitBtn = document.getElementById('Submit') // константа для кнопки - Выполнить
 
 resultElement.textContent = sum // вывод результата на экране
 console.log(resultElement.textContent) // вывод результата в консоли
 
+// создаем функцию, к которой подключаем кнопку Выполнить
 submitBtn.onclick = function () {
-    console.log('Hello Roman!')
+    console.log(Number(input1.value) + Number(input2.value))
 }
 
