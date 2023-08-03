ac950959fd50a4133d8c761f25e87d829e6c771c
Roman <11-roman@mail.ru>
2023-08-03 09:22:16 +0300

    использую новую функцию для + и -

--- lesson_1.js
+++ lesson_1.js
@@ -4,19 +4,36 @@
 // num = 10 // если переменная есть, то заново let мы не пишем
 // alert (firstName) // агрессивный способ вывести информацию на экран
 //console.log ("Test:", firstName) // выводит информацию в консоль
+//console.log(typeof num) // проверяет тип данных
 
-// создаем константы, которая подтягивает значение по id из файла html
+// создаем константы, которые подтягивают значения по id из файла html
 const resultElement = document.getElementById('result')
 const input1 = document.getElementById('input1') // первое число
 const input2 = document.getElementById('input2') // второе число
-const sum = Number(input1.value) + Number(input2.value) // константа суммы + изменение тепа на числовой
-const submitBtn = document.getElementById('Submit') // константа для кнопки - Выполнить
+const submitBtn = document.getElementById('Submit') // кнопка: Выполнить
+const plusBtn = document.getElementById('plus')
+const minusBtn = document.getElementById('minus')
 
-resultElement.textContent = sum // вывод результата на экране
-console.log(resultElement.textContent) // вывод результата в консоли
-
-// создаем функцию, к которой подключаем кнопку Выполнить
-submitBtn.onclick = function () {
-    console.log(Number(input1.value) + Number(input2.value))
+/* создаем функцию, чтобы использовать повторяющися код в разных функциях
+присваиваем значение - res, чтобы функция работала корректно */
+function resultt (res) {
+    submitBtn.onclick = function () { 
+        resultElement.textContent = res 
+        console.log(resultElement.textContent) 
+    }
 }
+
+/* мы подключили кнопку: Плюс, и вставили функцию для кнопки: Вычислить, 
+функции resultt присваиваем значение sum */
+plusBtn.onclick = function () {
+    const sum = Number(input1.value) + Number(input2.value)
+    resultt (sum)
+    }
+
+/* мы подключили кнопку: Минус, и вставили функцию для кнопки: Вычислить, 
+функции resultt присваиваем значение min */
+minusBtn.onclick = function () {
+    const min = Number(input1.value) - Number(input2.value)
+    resultt (min)
+    }
 
