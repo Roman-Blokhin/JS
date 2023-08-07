b035ec68db477797c92ddc899d54709ac052768a
Roman <11-roman@mail.ru>
2023-08-07 10:15:43 +0300

    знакомство с массивами

---
+++ notebook/lesson_2.js
@@ -0,0 +1,14 @@
+/* Создаем массив
+
+const array = [1, 2, 45, 789]
+console.log (array)
+console.log (array.length) // длина массива
+console.log (array[2]) // по индексу выводим элемент массива
+console.log (array[array.length - 1]) //последний элемент массива
+
+array[0] = 'becon' // заменили первый элемент в массиве
+console.log (array)
+*/
+
+
+
