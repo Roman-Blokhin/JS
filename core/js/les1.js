67e6c7ab9f2bd2358bea73c2f3410b324ae4bc10
Roman <11-roman@mail.ru>
2024-07-09 10:42:15 +0300

    JS. Переменные и константы

---
+++ core/js/les1.js
@@ -0,0 +1,16 @@
+// 0. используем в программе ES6
+'use strict'  
+
+// 1. let создает новую переменную
+let obj = 'Hello world';
+console.log(obj);
+
+// 2. допускается определение нескольких переменных в одной строке
+let age = 34, 
+	name = 'Roman', 
+	surname = 'Lost';
+
+age = 17; // можно менять значение переменной
+const PI = 3.1415;
+
+console.log(name, surname, age, PI) 
