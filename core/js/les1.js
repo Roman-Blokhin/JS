cfbe97092aa6ed3d5386683b10ffb3384e27dc66
Roman <11-roman@mail.ru>
2024-07-10 07:56:03 +0300

    JS. Браузерные скрипты: alert

--- core/js/les1.js
+++ core/js/les1.js
@@ -6,14 +6,20 @@ let obj = 'Hello world';
 console.log(obj);
 
 // 2. допускается определение нескольких переменных в одной строке
-let age = 34, 
+let ages = 34, 
 	name = 'Roman', 
 	surname = 'Lost';
 
-age = 17; // можно менять значение переменной
+ages = 17; // можно менять значение переменной
 const PI = 3.1415567;
 
-console.log(name, surname, age, PI) 
+console.log(name, surname, ages, PI) 
 
 // 3. можно узнать тип данных
-console.log(typeof age, typeof name)
+console.log(typeof ages, typeof name)
+
+// 4. скрипты, которые работают только в браузере
+// 4.1 модальное окно - закоментировал, так как может быть только одно всплывающее окно
+
+alert('Hello my friend/ Do you want to go for a walk?')  
+
